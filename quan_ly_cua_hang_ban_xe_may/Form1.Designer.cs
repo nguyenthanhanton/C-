1c3f8958bd1e566adce9e6811564f6273999f756
hieuhi814 <tranminhh2301@gmail.com>
2025-04-25 15:05:06 +0700

    xe dau ma` ban' ??

--- quan_ly_cua_hang_ban_xe_may/Form1.Designer.cs
+++ quan_ly_cua_hang_ban_xe_may/Form1.Designer.cs
@@ -35,24 +35,25 @@
             this.asdaweqweToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(349, 150);
+            this.label1.Location = new System.Drawing.Point(393, 188);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(136, 16);
+            this.label1.Size = new System.Drawing.Size(166, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nguyeen thanh an ton";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(228, 194);
+            this.button1.Location = new System.Drawing.Point(256, 242);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(67, 18);
+            this.button1.Size = new System.Drawing.Size(75, 22);
             this.button1.TabIndex = 1;
             this.button1.Text = "12323123";
             this.button1.UseVisualStyleBackColor = true;
@@ -63,42 +64,55 @@
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.asdaweqweToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(157, 28);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(177, 36);
             // 
             // asdaweqweToolStripMenuItem
             // 
             this.asdaweqweToolStripMenuItem.Name = "asdaweqweToolStripMenuItem";
-            this.asdaweqweToolStripMenuItem.Size = new System.Drawing.Size(156, 24);
+            this.asdaweqweToolStripMenuItem.Size = new System.Drawing.Size(176, 32);
             this.asdaweqweToolStripMenuItem.Text = "asdaweqwe";
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(104, 58);
+            this.checkBox1.Location = new System.Drawing.Point(117, 72);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(95, 20);
+            this.checkBox1.Size = new System.Drawing.Size(113, 24);
             this.checkBox1.TabIndex = 3;
             this.checkBox1.Text = "checkBox1";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(84, 180);
+            this.button2.Location = new System.Drawing.Point(94, 225);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(8, 8);
+            this.button2.Size = new System.Drawing.Size(9, 10);
             this.button2.TabIndex = 4;
             this.button2.Text = "asdas";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(332, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(353, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "CON CẶC XE MÁY ĐÂU MÀ BÁN HẢ MẤY CHA";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "xX";
             this.contextMenuStrip1.ResumeLayout(false);
@@ -115,6 +129,7 @@
         private System.Windows.Forms.ToolStripMenuItem asdaweqweToolStripMenuItem;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
     }
 }
 
